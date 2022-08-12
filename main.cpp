9df673fea307c7e39aa2d4704b9413f0836ee4df
wwwxkz <wxkzwin@gmail.com>
2022-08-12 10:48:18 -0300

    Fixed horizontal bar reversed display

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <tuple>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,6 +13,7 @@ void display(int width, char border, char direction, vector<string> frame) {
   for (int line = 0; line < frame.size(); line++) {
     // Calc blank spots removing content from display width
     int blank = width - frame[line].length();
+    // Align content R, L, C using blank
     switch (direction) {
     case 'r':
       cout << endl << string(blank, ' ') + frame[line];
@@ -32,18 +34,39 @@ void display(int width, char border, char direction, vector<string> frame) {
 
 vector<string> horizontal_bar(vector<tuple<int, string>> columns) {
   vector<string> frame;
-
+  // Get each column
   for (int column = 0; column < columns.size(); column++) {
-    for (int row = 0; row < get<0>(columns[column]); row++) {
-      frame.push_back("#");
+    int last_line = 0;
+    // Get column '#' value
+    int column_value = get<0>(columns[column]);
+    // Loop in frame adding '#' until frame.size() is reached
+    for (int line = 0; line < frame.size(); line++) {
+      // When column value is completely added, break, even if frame.size() is not reached
+      if(line >= column_value) {
+        break;
+      }
+      frame[line] += "#";
+      last_line++;
     }
-  } 
-
-  return frame;
+    // If frame.size() reached but not all column values add push_back until it completes
+    while (last_line < column_value) {
+      frame.push_back("#");
+      last_line++;
+    }
+  }
+  // Reverse frame as bar starts from bottom to top and not the oposite
+  vector<string> frame_reversed;
+  int frame_size = frame.size();
+  for (int line = 0; line < frame_size; line++) {
+    frame_reversed.push_back(frame[frame_size - line - 1]);
+  }
+  return frame_reversed;
 }
 
 vector<string> vertical_bar(vector<tuple<int, string>> columns, int align_by_label) {
   vector<string> frame;
+  // Align label text using blank spaces as padding
+  // Find largest label and follow adding blank spaces to subsequent labels
   int largest_label = 0;
   if (align_by_label == 1) {
       for (int column = 0; column < columns.size(); column++) {
@@ -71,7 +94,7 @@ void example_horizontal_bar() {
     make_tuple(2, "testing")
   };
   vector<string> h = horizontal_bar(bar);
-  display(display_w, *"-", 'c', h);
+  display(display_w, *"-", 'l', h);
 }
 
 void example_vertical_bar() {
